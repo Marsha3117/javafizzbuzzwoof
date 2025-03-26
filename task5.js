8204eb51d085e5f48f613e5f2a6503e8582ffce5
Marsha3117 <majoramos218@gmail.com>
2025-03-26 12:29:38 -0500

    v3

--- task5.js
+++ task5.js
@@ -1,5 +1,37 @@
 // Refer to Task 5 in your Instructions to complete this task
+const prompt = require ("prompt-sync")();
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Five!");
-  };
+let n = parseInt(prompt("Ingresa un número: "));
+
+for (let i = 0; i < n; i++) {
+  if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+    console.log("FizzBuzzWoof");
+  }
+
+  else if (i % 3 == 0 && i % 7 == 0) {
+    console.log("FizzWoof")
+  }
+
+  else if (i % 5 == 0 && i % 7 == 0) {
+    console.log("BuzzWoof")
+  }
+  else if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz")
+  }
+
+  else if (i % 3 == 0) {
+    console.log("Fizz")
+  }
+  
+  else if (i % 5 == 0) {
+    console.log("Buzz")
+  }
+
+  else if (i % 7 == 0) {
+    console.log("Woof")
+  }
+
+  else{
+    console.log(i)
+  }
+};
